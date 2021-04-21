ffb8e2640aa3968a65df2be9c3212c2657aaf738
makcsholonik <makcsholonik@gmail.com>
2021-04-21 22:45:30 +0300

    String repeat

---
+++ kyu_8/String_repeat.js
@@ -0,0 +1,9 @@
+//String repeat
+//Write a function called repeatStr which repeats the given string string exactly n times.
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+}
+
+//repeatStr = (n, s) => s.repeat(n)
+
