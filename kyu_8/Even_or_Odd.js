9fe304c14dc1b9fb4e7fe50548e4199af08ac4a4
makcsholonik <makcsholonik@gmail.com>
2021-07-15 22:59:24 +0300

    Leap Years

---
+++ kyu_8/Even_or_Odd.js
@@ -0,0 +1,4 @@
+//Even or Odd
+//Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+const even_or_odd = n => n % 2 === 0 ? "Even" : "Odd"
