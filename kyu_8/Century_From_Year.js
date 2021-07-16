d24ed5faf7b3a4d2b2b996622365a0cd362b6708
makcsholonik <makcsholonik@gmail.com>
2021-07-16 22:59:53 +0300

    Century From Year

---
+++ kyu_8/Century_From_Year.js
@@ -0,0 +1,7 @@
+//Century From Year
+
+/*
+The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+*/
+
+const century = year => Math.ceil(year / 100)
